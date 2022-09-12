22b868bbb8a27a7a65c078ad9461250f02c49438
Kenrich Huynh <Kenrich6118@gmail.com>
2022-09-12 12:26:01 -0500

    add decimal and stop user from having multiple decimals

--- calculator.js
+++ calculator.js
@@ -13,7 +13,10 @@ function division(a, b){
 }
 function operate(operator, a, b){
     if(operator === '+'){
-        return add(a, b);
+        if(a.includes('.') || b.includes('.')){
+            return add(parseFloat(a),parseFloat(b));
+        }
+        return add(parseInt(a), parseInt(b));
     }else if(operator === '-'){
         return sub(a, b);
     }else if(operator === '*'){
@@ -27,29 +30,61 @@ const sign = document.querySelectorAll('.sign');
 const display = document.querySelector('.display');
 const equal = document.querySelector('.equal');
 const clear = document.querySelector('.clear');
+const decimal = document.querySelector('.decimal')
 number.forEach(number => {
     number.addEventListener('click', () => {
-        display.textContent =  display.textContent + number.textContent;
+        if(displayText[displayText.length-2] === '+' || displayText[displayText.length-2] === '-' || displayText[displayText.length-2] === '*' || displayText[displayText.length-2] === '/'){
+            display.textContent = number.textContent;
+        }else{
+            display.textContent =  display.textContent + number.textContent;
+        }
         displayText = displayText + number.textContent;
     });
 });
 sign.forEach(sign => {
     sign.addEventListener('click', () => {
-        display.textContent =  display.textContent +  ' ' + sign.textContent + ' '; 
-        displayText = displayText + ' ' + sign.textContent + ' ';
+        if(displayText[displayText.length-1] !== sign.textContent){
+            display.textContent =  sign.textContent; 
+            displayText = displayText + ' ' + sign.textContent + ' ';
+        }
     });
 });
 equal.addEventListener('click', function(){
     const calculate = displayText.split(' ');
     let answer = 0;
     console.log(calculate);
-    for(i = 0;i < calculate.length;i++){
-        console.log(calculate[i-1]);
-        if(calculate[i] === '+' | calculate[i] === '-' | calculate[i] === '*' | calculate[i] === '/'){
-            console.log(operate(calculate[i], calculate[i-1], calculate[i+1]));
+    while(calculate.includes('+') || calculate.includes('-') || calculate.includes('*') || calculate.includes('/')){
+        for(i = 0; i < calculate.length;i++){
+            if(calculate[i] === '*'){
+                answer = operate(calculate[i], calculate[i-1], calculate[i+1]);
+                calculate.splice(i-1, 3, answer);
+            }else if(calculate[i] === '/'){
+                answer = operate(calculate[i], calculate[i-1], calculate[i+1]);
+                calculate.splice(i-1, 3, answer);
+            }
+        }
+        for(i = 0;i < calculate.length;i++){
+            if(calculate[i] === '+'){
+                answer = operate(calculate[i], calculate[i-1], calculate[i+1])
+                calculate.splice(i-1, 3, answer);
+
+            }else if(calculate[i] === '-'){
+                answer = operate(calculate[i], calculate[i-1], calculate[i+1])
+                calculate.splice(i-1, 3, answer);
+            }
         }
     }
+    display.textContent = answer;
+    displayText = answer;
     });
 clear.addEventListener('click', () => {
     display.textContent = '';
+    displayText = '';
+})
+
+decimal.addEventListener('click', ()=>{
+    if(!display.textContent.includes('.')){
+        display.textContent = display.textContent + '.';
+        displayText += '.';
+    }
 })
